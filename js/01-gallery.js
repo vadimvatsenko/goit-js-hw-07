2971ce04ddbb81925343ed51e07734eb5a382873
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-09-03 18:41:33 +0300

    fix task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -43,7 +43,6 @@ function onParentClick(evt) {
   };
   ligthBoxShow.close();
 });
-
 };
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const getGallery = document.querySelector('.gallery');
+
+const getGalleryItems = galleryItems.map(({ preview, original, description }) => {
+    console.log(preview);
+    return `<a class="gallery__item"  href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+                />
+          </a>`
+}).join('');
+
+getGallery.insertAdjacentHTML("afterbegin", getGalleryItems);
+
+
+let lightbox = new SimpleLightbox('.gallery .gallery__item', {
+    heightRatio: 0.8,
+
+});
