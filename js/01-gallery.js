f51383af67692a6f494f9e6c008d028fefa42c9d
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-09-03 13:19:54 +0300

    start homework 7 js

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const getGallery = document.querySelector('.gallery');
+
+const getGalleryItems = galleryItems.map(({ preview, original, description }) => {
+    console.log(preview);
+    return `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+          </div>`
+}).join('');
+
+getGallery.insertAdjacentHTML("afterbegin", getGalleryItems);
+
+getGallery.addEventListener('click', onParentClick);
+
+function onParentClick(evt) {
+  // запрет перехода по ссылке
+  evt.preventDefault();
+  console.log('onParentClick -> evt.target', evt.target);
+  // если не равно классу - пропустить
+  if (evt.target.className !== 'gallery__image') {
+    return;
+  }
+  // ссылка на текущий data-source
+  basicLightbox.create(`
+		<img src = '${evt.target.dataset.source}' alt = '${evt.target.alt}' width = 1140/>
+	`).show()
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
